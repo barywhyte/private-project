1a29c7ab9c6371d619c8985cca07188ae0f76da9
Seun Ore <seun.ore@andela.com>
2025-05-30 21:26:15 +0100

    separate codes into branches.

--- app/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from api import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('health', views.health),
-    path('info', views.info),
-]
