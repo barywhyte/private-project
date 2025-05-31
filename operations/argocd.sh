1e7e9a3319d4412b0f1394624f25b989732bbea2
Seun Ore <seun.ore@andela.com>
2025-05-31 13:38:46 +0100

    setup argocd for automated deploy

---
+++ operations/argocd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+kubectl create namespace argocd
+
+
+helm upgrade --install argocd argo/argo-cd \
+  --namespace argocd \
+  --set global.domain=argocdbary.duckdns.org \
+  --set server.ingress.enabled=true \
+  --set server.ingress.ingressClassName=nginx \
+  --set server.ingress.annotations."nginx\.ingress\.kubernetes\.io/force-ssl-redirect"="true" \
+  --set server.ingress.annotations."nginx\.ingress\.kubernetes\.io/ssl-passthrough"="true" \
+  --set server.ingress.annotations."cert-manager\.io/cluster-issuer"=letsencrypt-prod \
+  --set server.ingress.tls=true
