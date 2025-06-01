99ab65083a0625f3cf7696d4de7576d5bffb5580
Seun Ore <seun.ore@andela.com>
2025-06-01 23:17:41 +0100

    app version 4

--- app/api/views.py
+++ app/api/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def health(request):
-    return JsonResponse({"status": "ok"})
+    return JsonResponse({"status": "okay"})
 
 def info(request):
-    return JsonResponse({"app": "Django API", "version": "1.3.0"})
+    return JsonResponse({"app": "Django API", "version": "1.4.0"})
 
