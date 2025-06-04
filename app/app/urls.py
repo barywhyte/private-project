cc7a65caea6a055d41973570ce94bc252e320f9b
Seun Ore <seun.ore@andela.com>
2025-06-04 13:29:34 +0100

    add django prometheus requirements.

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from api import views
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health', views.health),
     path('info', views.info),
+    path('', include('django_prometheus.urls')),  # ← this adds /metrics
 ]
