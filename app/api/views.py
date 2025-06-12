fe18700b73d7b1e613195b5c66caf1af5f29ab35
Seun Ore <seun.ore@andela.com>
2025-06-12 16:41:44 +0100

    test error

--- app/api/views.py
+++ app/api/views.py
@@ -4,9 +4,9 @@ from django.http import JsonResponse
 
 def health(request):
     # Simulate failure
-    return JsonResponse({"status": "not ok!"}, status=500)
+    return JsonResponse({"status": "not ok!"}, status=503)
     #return JsonResponse({"status": "ok!"})
 
 def info(request):
-    return JsonResponse({"app": "Django API", "version": "2.2.0"})
+    return JsonResponse({"app": "Django API", "version": "2.3.0"})
 
