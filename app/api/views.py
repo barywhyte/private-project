e31f930efba4c3c2823076f8e7efaa13e09b48a9
Seun Ore <seun.ore@andela.com>
2025-06-13 00:59:31 +0100

    remove progressdeadline

--- app/api/views.py
+++ app/api/views.py
@@ -4,10 +4,10 @@ from django.http import JsonResponse
 
 def health(request):
     # Simulate failure
-    return JsonResponse({"status": "temporarily not ready"}, status=503)
+    #return JsonResponse({"status": "temporarily not ready"}, status=503)
 
-    #return JsonResponse({"status": "ok!"})
+    return JsonResponse({"status": "ok!"})
 
 def info(request):
-    return JsonResponse({"app": "Django API", "version": "3.2.0"})
+    return JsonResponse({"app": "Django API", "version": "3.1.0"})
 
