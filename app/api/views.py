a02bf907d83fb65e439435e0ae8ee12ef35925ba
Seun Ore <seun.ore@andela.com>
2025-06-13 01:51:40 +0100

    return

--- app/api/views.py
+++ app/api/views.py
@@ -4,9 +4,9 @@ from django.http import JsonResponse
 
 def health(request):
     # Simulate failure
-    return JsonResponse({"status": "temporarily not ready"}, status=500)
+    #return JsonResponse({"status": "temporarily not ready"}, status=500)
 
-    #return JsonResponse({"status": "ok!"})
+    return JsonResponse({"status": "ok!"})
 
 def info(request):
     return JsonResponse({"app": "Django API", "version": "3.2.0"})
