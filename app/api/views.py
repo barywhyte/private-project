c0b728c487ca9c87a019fd6c961bf5230a1a189e
Seun Ore <seun.ore@andela.com>
2025-06-15 23:29:47 +0100

    version 4.3

--- app/api/views.py
+++ app/api/views.py
@@ -7,4 +7,4 @@ def health(request):
     return JsonResponse({'status': 'all good!'}, status=200)
 
 def info(request):
-    return JsonResponse({"app": "Django API", "version": "4.2.0"})
+    return JsonResponse({"app": "Django API", "version": "4.3.0"})
