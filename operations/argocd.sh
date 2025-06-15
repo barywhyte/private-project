6b120f3cafa83c1823b9cabcf4d15d7e64d873a2
Seun Ore <seun.ore@andela.com>
2025-06-15 22:12:02 +0100

    adding loki for loggin

--- operations/argocd.sh
+++ operations/argocd.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-# Install argocd and argocd-image-updater
+
+set -e
+
+# Create namespaces if they don't exist
+kubectl get namespace argocd >/dev/null 2>&1 || kubectl create namespace argocd
+kubectl get namespace argo-rollouts >/dev/null 2>&1 || kubectl create namespace argo-rollouts
+kubectl get namespace monitoring >/dev/null 2>&1 || kubectl create namespace monitoring
+
+# Install ArgoCD Image Updater
 kubectl apply -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml -n argocd
 
+# Add Helm repositories
 helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
-kubectl create namespace argocd
-
-
+# Install ArgoCD via Helm
 helm upgrade --install argocd argo/argo-cd \
   --namespace argocd \
   --set global.domain=argocdbary.duckdns.org \
@@ -18,17 +26,21 @@ helm upgrade --install argocd argo/argo-cd \
   --set server.ingress.annotations."cert-manager\.io/cluster-issuer"=letsencrypt-prod \
   --set server.ingress.tls=true
 
-# Install argocd-rollouts
-kubectl create namespace argo-rollouts
+# Install Argo Rollouts
 kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
 
+# Install Loki Stack (Loki backend with gateway)
+helm upgrade --install loki grafana/loki -n monitoring
 
-# Install Loki for logging
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
 
-helm install loki grafana/loki --namespace monitoring --create-namespace
+# Install Promtail with proper multi-tenancy config
+helm upgrade --install promtail grafana/promtail \
+  --namespace monitoring \
+  --values promtail-values.yml
 
-# Install Promtail for log collection
-helm install promtail grafana/promtail --namespace monitoring --set "loki.serviceName=loki"
+# Print Grafana Loki info
+echo "Loki installed. Configure Grafana data source as:"
+echo "URL: http://loki-gateway.monitoring.svc.cluster.local/loki"
+echo "Tenant ID: foo"
 
+echo "Promtail installed and properly configured for multi-tenancy"
