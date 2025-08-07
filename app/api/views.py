ac727cf61b2587feef7c210713e0d72f8e73d048
Seun Ore <seun.ore@andela.com>
2025-08-07 14:14:47 +0100

    version 6.1 plus additional security

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.http import HttpResponse
 from .forms import PostForm, RawForm
@@ -11,7 +11,7 @@ def health(request):
     return JsonResponse({'status': 'all good!'}, status=200)
 
 def info(request):
-    return JsonResponse({"app": "Django API", "version": "6.0.0"})
+    return JsonResponse({"app": "Django API", "version": "6.1.0"})
 
 def home(request):
 
@@ -40,19 +40,19 @@ def create_post(request):
 
 def post_details(request, id):
 
-    try:
-        obj = Api.objects.get(id=id)
-        post_content = {
-            "title": obj.title,
-            "content": obj.contents,
-            "price": obj.price,
-            "author": obj.author,
-            "description": obj.description,
-            "featured": obj.featured
-        }
-        return render(request, 'index.html', post_content)
-    except Api.DoesNotExist:
-        return HttpResponse("Post not found", status=404)
+    post = get_object_or_404(Api, id=id)
+    return render(request, 'post_details.html', {'post': post})
+
+def list_all_posts(request):
+    posts = Api.objects.all()
+    return render(request, 'list_posts.html', {'posts': posts})
+
+def delete_post(request, id):
+    post = get_object_or_404(Api, id=id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+    return render(request, 'delete_post.html', {'post': post})
 
 
 
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import home, health, info, about_me, post_details, create_post
+from api.views import home, health, info, about_me, post_details, create_post, delete_post,list_all_posts
 from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('health', health),
-    path('info', info),
+    path('admin/', admin.site.urls, name='admin'),
+    path('health', health, name='health'),
+    path('info', info, name='info'),
     path('', home, name='home'),  # This will serve the index.html at the root URL
-    path('about/', about_me),
+    path('about/', about_me, name='about_me'),
     path('', include('django_prometheus.urls')),  # ← this adds /metrics
-    path('post/', post_details),
-    path('new_post/', create_post),
-    path('post/<int:id>/', post_details),
+    #path('post/', list_all_posts),
+    path('new_post/', create_post, name='create_post'),
+    path('post/<int:id>/', post_details, name='post_details'),
+    path('post/<int:id>/delete', delete_post, name='delete_post'),
+    path('all_posts/', list_all_posts, name='list_all_posts'),
 ]
